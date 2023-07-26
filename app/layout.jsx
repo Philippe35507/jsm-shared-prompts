34e55d7282783557d162dd575443b580dd74f2eb
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-27 00:38:39 +0100

    Creating Home + RootLayout + edit jsconfig.json

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promptopia",
+  descritpion: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div ClassName="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
