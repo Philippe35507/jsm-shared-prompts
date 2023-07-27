d331abf8212d73e15c3d39830b9896acb915bc95
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-27 22:28:07 +0100

    Desktop navigation: "Create Post" button

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import Image from "next/image";
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -16,6 +18,19 @@ const Nav = () => {
         />
         <p className="logo_text">Promptopia</p>
       </Link>
+
+      {/* Desktop navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </nav>
   );
 };
