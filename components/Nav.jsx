473cf8d7cd9e8c298706d84efcc5c473b9693799
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-27 23:15:43 +0100

    Desktop navigation: "Sign In" button (getProviders)
    (not showing for the moment)

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,10 +2,22 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { signOut } from "next-auth/react";
+import { useState, useEffect } from "react";
+import { SignIn, signOut, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const isUserLoggedIn = false;
+
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+
+      setProviders(response);
+    };
+    setProviders();
+  }, []);
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -41,7 +53,19 @@ const Nav = () => {
             </Link>
           </div>
         ) : (
-          <></>
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => SignIn(provider.id)}
+                  className="black-btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
         )}
       </div>
     </nav>
