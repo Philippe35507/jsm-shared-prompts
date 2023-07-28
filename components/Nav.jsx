01e1f6bb85aab5d6757fdf3d096feba6e56f5446
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-28 01:39:58 +0100

    SessionProvider + Provider layout

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import { signIn, signOut, getProviders } from "next-auth/react";
+import { signIn, signOut, getProviders, session } from "next-auth/react";
 
 const Nav = () => {
   const isUserLoggedIn = true;
